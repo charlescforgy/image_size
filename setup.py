85860c94cdfdbc781e1b1ff6133589a46874dd70
Charles C. Forgy <ccforgy@gmail.com>
2020-12-26 20:29:46 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+s = setup(
+    name="image-sizes",
+    version="1.0.0",
+    license="MIT",
+    description="Creates Python Dictionary of image dimensions.",
+    url="",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires = ">= 3.4",
+    author="Charles C. Forgy",
+    author_email="ccforgy@gmail.com",
+    )
